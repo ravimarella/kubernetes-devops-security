1031301c774aa7478ee48fcd14a438db87d571cb
ravi39094 <ravikiran.marella@tietoevry.com>
2022-06-22 15:49:54 +0530

    update configuration

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -1,7 +1,8 @@
 package com.devsecops;
 
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
